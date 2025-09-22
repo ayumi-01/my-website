b85d085abcb30afa0bef4c7668a1862f4ce7f829
ayumi-01 <mn1873788@gmail.com>
2025-09-22 21:13:31 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,67 @@
+/*スライダーここまで*/
+const track = document.querySelector('.gallery-track');
+const leftBtn = document.querySelector('.arrow.left');
+const rightBtn = document.querySelector('.arrow.right');
+const thumbs = document.querySelectorAll('.thumb');
+const lightbox = document.getElementById('lightbox');
+const lightboxImg = document.getElementById('lightbox-img');
+const dots = document.querySelectorAll('.dot');
+
+let index = 0;
+const itemWidth = 410; // 画像幅 + gap
+
+rightBtn.addEventListener('click', () => {
+  index = Math.min(index + 1, thumbs.length - 1);
+  track.scrollTo({
+    left: index * itemWidth,
+    behavior: 'smooth'
+  });
+  updateDots();
+});
+
+leftBtn.addEventListener('click', () => {
+  index = Math.max(index - 1, 0);
+  track.scrollTo({
+    left: index * itemWidth,
+    behavior: 'smooth'
+  });
+  updateDots();
+});
+
+function updateDots() {
+  dots.forEach(dot => dot.classList.remove('active'));
+  if (dots[index]) {
+    dots[index].classList.add('active');
+  }
+}
+/*スラーダーここまで*/
+thumbs.forEach(thumb => {
+  thumb.addEventListener('click', () => {
+    lightboxImg.src = thumb.src;
+    lightbox.style.display = 'flex';
+  });
+});
+
+lightbox.addEventListener('click', () => {
+  lightbox.style.display = 'none';
+});
+
+/*ハンバーガーメニュここから*/
+const hamburger = document.getElementById('hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+/*スラーダー2個めここから*/
+    $(document).ready(function(){
+      $('.slider').slick({
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000
+      });
+    });
+
+/*スラーダー2個めここまで*/
